aa79777c4490150ae29ca0314800676c6e220841
linkouth <34433986+linkouth@users.noreply.github.com>
2019-03-04 13:29:03 +0300

    Update caesarCipher.py
    update CaesarCipher

--- caesarCipher.py
+++ caesarCipher.py
@@ -1,30 +1,26 @@
 if __name__ == '__main__':
+    message = input('Enter the message: ')
+    key = input('Enter the key: ')
+    mode = input('Encrypt or decrypt: ')
 
-    message = input()
-    key = int(input())
-    mode = input('encrypt or decrypt')
+    LETTERS = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФЧЦЧШЩЪЫЬЭЮЯ0123456789,.!,:; '
 
-    LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    key_len = len(key)
+    letters_len = len(LETTERS)
 
     translated = ''
 
     message = message.upper()
 
-    for symbol in message:
+    for i, symbol in enumerate(message):
         if symbol in LETTERS:
             num = LETTERS.find(symbol)
+            key_num = LETTERS.find(key[i % key_len])
             if mode == 'encrypt':
-                num = num + key
+                num = (num + key_num) % letters_len
             elif mode == 'decrypt':
-                num = num - key
-
-            if num >= len(LETTERS):
-                num = num - len(LETTERS)
-            elif num < 0:
-                num = num + len(LETTERS)
-
+                num = (num - key_num) % letters_len
             translated = translated + LETTERS[num]
-
         else:
          translated = translated + symbol
 
